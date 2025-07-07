c77382425bc2be1e28a0d82cd9569e42ac47c2b7
Neha C J <cjneha890@gmail.com>
2025-07-07 13:15:08 +0530

    added button fn

--- profile-card/src/App.jsx
+++ profile-card/src/App.jsx
@@ -1,14 +1,16 @@
-import { useState } from 'react'
-
 import UserProfile from './Components/UserProfile'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const user={
+    name: "Kylo Ren",
+    bio: "A dark warrior strong with the Force, Kylo Ren commands First Order missions with a temper as fiery as his unconventional lightsaber",
+    email: "kyloren@starwars.com",
+    location: "Starkiller Base"
+  }
   return (
     <div className='flex flex-col items-center'> 
       <h1 className='text-3xl p-3' >USER PROFILE CARD</h1>
-      <UserProfile />
+      <UserProfile{...user} />
     </div>
   )
 }
--- profile-card/src/Components/UserProfile.jsx
+++ profile-card/src/Components/UserProfile.jsx
@@ -1,15 +1,26 @@
 import profileImg from './assets/kyloren.jpeg'
-const UserProfile = () =>{
-    return(
-        <div class="PCard p-2 m-2 border rounded-lg shadow-lg flex flex-col justify-center items-center gap-2 w-[400px] bg-black text-white hover:scale-105 transition-transform duration-300 ease-in-out">
-            <div></div>
-            <div>
-                <img className='w-[200px] h-auto rounded border-white' src={profileImg} alt="profile img"/>
-            </div>
-            <div className="text-2xl font-semibold ">Kylo Ren</div>
-            <div className="profile-bio text-center">A dark warrior strong with the Force, Kylo Ren commands First Order missions with a temper as fiery as his unconventional lightsaber.</div>
+import { useState } from 'react'
 
-            <button className='border rounded p-2 bg-black text-red-300'>Know More</button>
+
+const UserProfile = ({name, bio, email, location}) =>{
+    const [showDetails,setShowDetails]= useState(false)
+    return(
+        <div className="PCard p-2 m-2 border rounded-lg shadow-lg flex flex-col justify-center items-center gap-2 w-[400px] bg-black text-white hover:scale-105 transition-transform duration-300 ease-in-out">
+            <div className='bg-gray-500 w-full h-[100px]'></div>
+            <div>
+                <img className='mt-[-75px] p-1 bg-black rounded-full' src={profileImg} alt="profile img"/>
+            </div>
+            <div className="text-2xl font-semibold ">{name}</div>
+            <div className="profile-bio text-center text-gray-400">{bio}</div>
+
+            {showDetails && (
+                <div className='text-gray-200'>
+                    <p>Email: {email}</p>
+                    <p>Location: {location}</p>
+                </div>
+            )}
+
+            <button className='border rounded p-2 bg-black text-red-300' onClick={()=> setShowDetails((prev) =>!prev)}>{showDetails ? "Hide": "Know More"}</button>
         </div>
     )
 }
